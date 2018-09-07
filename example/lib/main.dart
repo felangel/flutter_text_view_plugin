12b593a3e37e1719e002b4a26b123c7fd2a5fcf5
Felix Angelov <felangelov@gmail.com>
2018-09-07 01:44:21 -0500

    tweaked example to demonstrate a more complex layout

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,12 +8,24 @@ class TextViewExample extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Flutter TextView example')),
-        body: TextView(
-          onTextViewCreated: _onTextViewCreated,
-        ));
+        body: Column(children: [
+          Center(
+              child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 30.0),
+                  width: 130.0,
+                  height: 100.0,
+                  child: TextView(
+                    onTextViewCreated: _onTextViewCreated,
+                  ))),
+          Expanded(
+              flex: 3,
+              child: Container(
+                  color: Colors.blue[100],
+                  child: Center(child: Text("Hello from Flutter!"))))
+        ]));
   }
 
   void _onTextViewCreated(TextViewController controller) {
-    controller.setText('Hello World!');
+    controller.setText('Hello from Android!');
   }
 }
